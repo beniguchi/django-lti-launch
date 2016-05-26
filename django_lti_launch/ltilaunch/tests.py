17b8ede7f74aed4ad6b4dccf5a40ecd4ad53a0d4
Steven Huwig <steve.huwig@unizin.org>
2016-05-26 14:17:22 -0400

    Refactor test code

--- django_lti_launch/ltilaunch/tests.py
+++ django_lti_launch/ltilaunch/tests.py
@@ -30,20 +30,7 @@ class LaunchViewTestCase(TestCase):
             signature_type=oauthlib.oauth1.SIGNATURE_TYPE_BODY)
         params = {"tool_consumer_instance_guid": self.guid,
                   "user_id": self.user_id}
-        uri, headers, body = oauth_signer.sign(
-            self.uri,
-            http_method="POST",
-            body=params,
-            headers={"Content-Type": "application/x-www-form-urlencoded"})
-        resp = self.client.post(
-            uri, body,
-            headers=headers,
-            secure=True,
-            content_type="application/x-www-form-urlencoded")
-        self.assertIsNotNone(resp, "response should not be None")
-        self.assertEquals(401, resp.status_code,
-                          "response status should be 401 Unauthorized")
-        self.assertNotIn('sessionid', resp.client.cookies)
+        self._failure(oauth_signer, params)
 
     def test_bad_signature(self):
         oauth_signer = oauthlib.oauth1.Client(
@@ -52,20 +39,7 @@ class LaunchViewTestCase(TestCase):
             signature_type=oauthlib.oauth1.SIGNATURE_TYPE_BODY)
         params = {"tool_consumer_instance_guid": self.guid,
                   "user_id": self.user_id}
-        uri, headers, body = oauth_signer.sign(
-            self.uri,
-            http_method="POST",
-            body=params,
-            headers={"Content-Type": "application/x-www-form-urlencoded"})
-        resp = self.client.post(
-            uri, body,
-            headers=headers,
-            secure=True,
-            content_type="application/x-www-form-urlencoded")
-        self.assertIsNotNone(resp, "response should not be None")
-        self.assertEquals(401, resp.status_code,
-                          "response status should be 401 Unauthorized")
-        self.assertNotIn('sessionid', resp.client.cookies)
+        self._failure(oauth_signer, params)
 
     def test_guid_mismatch(self):
         oauth_signer = oauthlib.oauth1.Client(
@@ -74,6 +48,9 @@ class LaunchViewTestCase(TestCase):
             signature_type=oauthlib.oauth1.SIGNATURE_TYPE_BODY)
         params = {"tool_consumer_instance_guid": "not the right thing",
                   "user_id": self.user_id}
+        self._failure(oauth_signer, params)
+
+    def _failure(self, oauth_signer, params):
         uri, headers, body = oauth_signer.sign(
             self.uri,
             http_method="POST",
@@ -98,27 +75,7 @@ class LaunchViewTestCase(TestCase):
             signature_type=oauthlib.oauth1.SIGNATURE_TYPE_BODY)
         params = {"user_id": self.user_id,
                   "music": "response"}
-        uri, headers, body = oauth_signer.sign(
-            self.uri,
-            http_method="POST",
-            body=params,
-            headers={"Content-Type": "application/x-www-form-urlencoded"})
-        resp = self.client.post(
-            uri, body,
-            headers=headers,
-            secure=True,
-            content_type="application/x-www-form-urlencoded")
-        self.assertIsNotNone(resp, "response should not be None")
-        users = LTIUser.objects.filter(lti_user_id=self.user_id)
-        self.assertEqual(1, len(users), "user should exist")
-        # check cool json stuff
-        results = LTIUser.objects.filter(
-            last_launch_parameters__music='response')
-        self.assertEqual(1, len(results),
-                         "should be able to search by LTI launch data")
-        self.assertRedirects(resp, '/', status_code=303,
-                             fetch_redirect_response=False)
-        self.assertIn('sessionid', resp.client.cookies)
+        self._successful(oauth_signer, params)
 
     def test_success(self):
         oauth_signer = oauthlib.oauth1.Client(
@@ -128,6 +85,9 @@ class LaunchViewTestCase(TestCase):
         params = {"tool_consumer_instance_guid": self.guid,
                   "user_id": self.user_id,
                   "music": "response"}
+        self._successful(oauth_signer, params)
+
+    def _successful(self, oauth_signer, params):
         uri, headers, body = oauth_signer.sign(
             self.uri,
             http_method="POST",
