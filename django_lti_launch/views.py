f079a136b1b64fbef08cc23e3fdaccab083fd0e6
Steven Huwig <steve.huwig@unizin.org>
2016-06-07 12:27:44 -0400

    Use the new LTIUser pk in example code

--- django_lti_launch/views.py
+++ django_lti_launch/views.py
@@ -1,6 +1,7 @@
 from django.core.urlresolvers import reverse
 from django.views.generic import TemplateView
 
+from ltilaunch import LTIUSER_SESSION_KEY
 from ltilaunch.models import LTIUser
 
 
@@ -10,7 +11,8 @@ class SuccessView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(SuccessView, self).get_context_data(**kwargs)
         launch_data = LTIUser.objects.get(
-            user=self.request.user).last_launch_parameters
+            pk=self.request.session[LTIUSER_SESSION_KEY]).last_launch_parameters
+        context['user'] = self.request.user
         context['launch_data'] = launch_data
         context['next_link'] = reverse('return')
         return context
