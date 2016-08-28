2249073c18bd6f50d110fac08aa7e4b2c449b12d
Steven Huwig <steve.huwig@unizin.org>
2016-08-28 11:05:55 -0400

    Small fixes to LTIToolProvider/ConfigView
    We should be able to leave the visibility blank in the admin screen.
    Also, let's make the URL end in .xml in case Canvas validates that
    for some reason.
    
    Fixes #6

--- ltilaunch/urls.py
+++ ltilaunch/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name = 'ltilaunch'
 urlpatterns = [
-    url(r'^config/(?P<slug>\w+)/$',
+    url(r'^config/(?P<slug>\w+)/config.xml$',
         views.ConfigView.as_view(template_name='config.xml'),
         name='lti_config_view')
 ]
