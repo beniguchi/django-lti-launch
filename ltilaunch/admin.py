31eb2cfd0b7f4c6787a14f11a434da238647941d
Steven Huwig <steve.huwig@unizin.org>
2016-09-14 17:39:07 -0400

    Messy version of key-value editor for LTI launch

--- ltilaunch/admin.py
+++ ltilaunch/admin.py
@@ -1,4 +1,9 @@
+import json
+
 from django.contrib import admin
+from django.contrib.postgres.fields import JSONField
+from django.forms import Widget
+from django.utils.safestring import mark_safe
 
 from .models import LTIToolConsumer, LTIToolProvider, LTIToolConsumerGroup, \
     LTIUser
@@ -36,9 +41,44 @@ class LTIToolProviderAdmin(admin.ModelAdmin):
         return form
 
 
+class LTILaunchParameterInputs(Widget):
+    def render(self, name, value, attrs=None):
+        data = json.loads(value)
+        ret = '<ul>'
+        for k, v in sorted(data.items()):
+            ctx = {'key': k,
+                   'value': v,
+                   'fieldname': name}
+            ret += '<li><label style="width: 300px; display: inline-block; ' \
+                   'text-align: ' \
+                   'right"> ' \
+                   '%(key)s</label><input type="hidden" ' \
+                   'name="json_key[%(fieldname)s]" value="%(key)s"><input ' \
+                   'style="width: 400px" ' \
+                   'type="text" ' \
+                   'name="json_value[%(' \
+                   'fieldname)s]" value="%(value)s"></li>' % ctx
+        ret += '</ul>'
+        return mark_safe(ret)
+
+    def value_from_datadict(self, data, files, name):
+        result = []
+        if (('json_key[%s]' % name) in data
+                and ('json_value[%s]' % name) in data):
+            keys = data.getlist("json_key[%s]" % name)
+            values = data.getlist("json_value[%s]" % name)
+            for key, value in zip(keys, values):
+                if len(key) > 0:
+                    result += [(key,value)]
+        return json.dumps(dict(result))
+
+
 class LTIUserAdmin(admin.ModelAdmin):
     list_display = ("person_name", "source_lms",
                     "last_launch_course_id")
+    formfield_overrides = {
+        JSONField: {'widget': LTILaunchParameterInputs}
+    }
 
 
 admin.site.register(LTIToolConsumer, LTIToolConsumerAdmin)
